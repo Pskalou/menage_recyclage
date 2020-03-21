d7ce3f4856ec9c9bfcee3cf611730844ec2babdb
Bouscadilla <bouscadilla@debiankal>
2020-03-21 05:23:43 +0100

    j'ai réussi à faire glisser 1 tete

--- Area2D.gd
+++ Area2D.gd
@@ -18,11 +18,57 @@ func _ready():
 #func _process(delta):
 #	pass
 
+#func _input(event):
+#	print(event.as_text())
+#	if event is InputEventMouseButton and event.is_pressed():
+#		print(event,"mouse button event at ", event.position)
+#		tete.set_frame(1)
+#		tete.set_scale(Vector2(1.2,1.2))
+#	else:
+#		tete.set_frame(0)
+#		tete.set_scale(Vector2(1,1))
+
+#
+#
+
+
+var is_over= false
+
+func _on_Area2D_mouse_entered():
+	is_over= true
+func _on_Area2D_mouse_exited():
+	is_over= false
+
+
+var dragging = false
+var delta_pos
+
+
 func _input(event):
-	if event is InputEventMouseButton and event.is_pressed():
-		print(event,"mouse button event at ", event.position)
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+		# commence à déplacer la tete
+		if is_over and !dragging and event.pressed:
+			dragging= true
+			delta_pos= event.position - position
+			print(dragging, event.position, position)
+		# arrête de déplacer la tete
+		if dragging and !event.pressed:
+			dragging= false
+			print(dragging)
+			
+	if dragging:
 		tete.set_frame(1)
 		tete.set_scale(Vector2(1.2,1.2))
+		position.x = event.position.x - delta_pos.x
+		position.y = event.position.y - delta_pos.y
+		if position.x < 100:
+			position.x= 100
+		if position.x> 950:
+			position.x= 950
+		if position.y < 100:
+			position.y= 100
+		if position.y > 500:
+			position.y= 500
 	else:
 		tete.set_frame(0)
 		tete.set_scale(Vector2(1,1))
